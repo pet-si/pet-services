35e0c05739bfce6f08862a181be3f89890e93636
Mateus Cardoso <151787016+mateus250402@users.noreply.github.com>
2026-05-17 17:18:04 -0300

    meio commit =)

---
+++ composeApp/src/commonMain/kotlin/ufsm/petsi/petservices/repository/interfaces/ReportInterface.kt
@@ -0,0 +1,12 @@
+package ufsm.petsi.petservices.repository.interfaces
+
+import kotlinx.coroutines.flow.Flow
+import ufsm.petsi.petservices.models.Report
+
+interface ReportInterface {
+    fun getReport() : Flow<Report?>
+    fun getAllReports(): Flow<List<Report>>
+    suspend fun insertReport(report: Report)
+    suspend fun updateReport(report: Report)
+    suspend fun deleteReport(report: Report)
+}
---
+++ composeApp/src/commonMain/kotlin/ufsm/petsi/petservices/repository/mappers/ReportMapper.kt
@@ -0,0 +1,5 @@
+package ufsm.petsi.petservices.repository.mappers
+
+import ufsm.petsi.petservices.models.Report
+import kotlinx.datetime.Instant
+import ufsmpetsipetservicesdatabase.Report as ReportEntity
